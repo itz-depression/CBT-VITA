dfe859a876ddf0420bc3c6b0072be1b342002a06
itz_Deprestion <cryan584@gmail.com>
2020-01-10 00:16:26 +0000

    Add files via upload

--- hcstudio.c
+++ hcstudio.c
@@ -14,7 +14,20 @@ int _sceDisplaySetFrameBufInternalForDriver(int fb_id1, int fb_id2, const SceDis
 		memcpy(&kfb, pParam, sizeof(SceDisplayFrameBuf));
 		blit_set_frame_buf(&kfb);
 		blit_set_color(0x00000000, 0x00ffffff);
-		blit_string(0, 0, "Cock and ball torture (CBT), penis torture or dick torture is a sexual activity involving application of pain or constriction to the penis or testicles. This may involve directly painful activities, such as genital piercing, wax play, genital spanking, squeezing, ball-busting, genital flogging, urethral play, tickle torture, erotic electrostimulation, kneeing or kicking.[1] The recipient of such activities may receive direct physical pleasure via masochism, or emotional pleasure through erotic humiliation, or knowledge that the play is pleasing to a sadistic dominant. Many of these practices carry significant health risks.[2]");
+		blit_string(0, 0, "Cock and ball torture (CBT), penis torture");
+		blit_string(0, 20, "is a sexual activity involving application");
+		blit_string(0, 40, "of pain or constriction to the penis or");
+		blit_string(0, 60, "testicles.This may involve directly painful");
+		blit_string(0, 80, "activities, such as genital piercing, wax play");
+		blit_string(0, 100, "genital spanking, squeezing, ball-busting, genital flogging,");
+		blit_string(0, 120, "urethral play, tickle torture, erotic");
+		blit_string(0, 140, "electrostimulation, kneeing or kicking");
+		blit_string(0, 160, "The recipient of such activities may receive");
+		blit_string(0, 180, "direct physical pleasure via masochism, or emotional pleasure");
+		blit_string(0, 200, "through erotic humiliation, or knowledge that the play is");
+		blit_string(0, 220, "pleasing to a sadistic dominant. Many of these practices");
+		blit_string(0, 240, "carry significant health risks");
+		
 	}
 	return TAI_CONTINUE(int, ref_hook0, fb_id1, fb_id2, pParam, sync);
 }
